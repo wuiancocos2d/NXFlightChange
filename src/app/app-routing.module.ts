e9fbd8eb573edbfe446d3b749f5378e557f0f8ef
wuiancocos2d <wuiancocos2d@gmail.com>
2018-10-19 20:38:45 +0800

    nom

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+
   ],
   declarations: []
 })
